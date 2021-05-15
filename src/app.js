2d98426864b4d3556e6381ee7995a69666a1bc20
biswaroop1547 <biswaroop08@gmail.com>
2021-05-15 11:06:17 +0530

    added jumbotron container

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import React, { Component }  from 'react';
+
+import jumboData from './fixtures/jumbo'
 import Jumbotron from '../src/components/jumbotron'
 
 export default function App() {
   return (
-    <Jumbotron />
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <p>Hello</p>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
 
--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,11 +1,16 @@
 import react from 'react'
 
-import {Inner} from './styles/jumbotron'
+import {Container, Inner} from './styles/jumbotron'
 
-export default function Jumbotron({}) {
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
     return (
-        <Inner>
+        <Inner direction={direction}>
             <p>Hello me from Inner Jumbo</p>
         </Inner>
     )
 }
+
+Jumbotron.Container = ({children, ...restProps}) => (<Container {...restProps}> {children} </Container>);
+// or -> Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
+    // return <Container {...restProps}> {children} </Container>;
+// }
